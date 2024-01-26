bd0f0fe9bbc54ac93542dc8dd35ce1b2f52ea42c
riponsheikhh <riponsheikh3211@gmail.com>
2024-01-26 22:51:58 +0600

    5.6e

--- Module-5/5-3_Char.c
+++ Module-5/5-3_Char.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 int main ()
 {
-    char a;
+    char a ;
     scanf("%c", &a);
-    if (a||a)
+    if(a>= 'a' && a<='z')
+    {
+        int ans = a-32;
+        printf ("%c", ans);
+    }
+    else 
+    {
+        int ans = a+32;
+        printf ("%c",ans);
+    }
     
-
 
     return 0;
 }
--- Module-5/5-4_First_digit.c
+++ Module-5/5-4_First_digit.c
@@ -1,16 +1,35 @@
+// #include<stdio.h>
+// int main ()
+// {
+//     int i;
+//     scanf("%1d", & i);
+
+//     if (i%2==0)
+//     {
+//         printf("EVEN", i);
+//     }
+//     else
+//     {
+//         printf ("ODD");
+//     }
+
+//     return 0;
+// }
+
+
 #include<stdio.h>
 int main ()
 {
-    int i;
-    scanf("%1d", & i);
-
-    if (i%2==0)
+    int x;
+    scanf ("%d", &x);
+    int ans = x/1000;
+    if (ans%2==0)
     {
-        printf("Even", i);
+        printf ("EVEN");
     }
     else
     {
-        printf ("odd");
+        printf("ODD");
     }
 
     return 0;
